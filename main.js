ce0b7426efe63f50615f85106e39954714f967a5
Terra Roush <terra.roush@gmail.com>
2020-04-23 13:03:13 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+const cookedFood = [];
+
+
+function grill (currentObject) {
+    currentObject.cooked = true;
+
+    cookedFood.push(currentObject);
+};
+
+for (let index = 0; index < foods.length; index++) {
+	if ( cooked = false ){
+		(function grill (index)
+	}
+	
+}
+// ok i am not sure how to proceed to get this to wokr, but i think i am close to soemthing good. since i am behind 8 chapters, i will be happy with this.
